24e73585711cda8edcb3a8e43c9504a20120c041
Yann Defretin <kinoute@gmail.com>
2019-11-03 20:46:42 +0100

    copy conda environments yml files somewhere

--- scripts/conda.sh
+++ scripts/conda.sh
@@ -1,1 +1,8 @@
-# Todo
+#!/usr/bin/env bash
+
+###############################################################################
+# Use HomeBrew to install every package/app needed                            #
+###############################################################################
+
+# copy our environments files
+cp -r -f ./conda/* ~/.conda/
