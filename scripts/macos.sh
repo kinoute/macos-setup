7cbae32c4f8171b82c8e3868ff55b88e96d9b7a2
Yann Defretin <kinoute@gmail.com>
2019-11-07 11:00:05 +0100

    keep shadows when taking screenshots

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -106,9 +106,6 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
 
-# Disable shadow in screenshots
-defaults write com.apple.screencapture disable-shadow -bool true
-
 # Save screenshots to the Desktop
 defaults write com.apple.screencapture location -string "$\{HOME\}/Desktop"
 
