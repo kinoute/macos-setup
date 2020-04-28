5a9cc6800f91fff32c2de8c075d1b0bb7d5ab46d
Yann Defretin <kinoute@gmail.com>
2020-04-28 13:57:22 +0200

    use git-delta instead of diff-so-fancy

--- scripts/git.sh
+++ scripts/git.sh
@@ -19,20 +19,3 @@ if [ ! -f  ~/.gitconfig ]; then
     git config --global user.email "$email"
 
 fi
-
-# Diff colors
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-
-git config --global color.ui true
-
-git config --global color.diff-highlight.oldNormal    "red bold"
-git config --global color.diff-highlight.oldHighlight "red bold 52"
-git config --global color.diff-highlight.newNormal    "green bold"
-git config --global color.diff-highlight.newHighlight "green bold 22"
-
-git config --global color.diff.meta       "11"
-git config --global color.diff.frag       "magenta bold"
-git config --global color.diff.commit     "yellow bold"
-git config --global color.diff.old        "red bold"
-git config --global color.diff.new        "green bold"
-git config --global color.diff.whitespace "red reverse"
