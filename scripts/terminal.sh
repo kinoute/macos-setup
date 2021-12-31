7d2bfbee80d796f9b15e8f04782cfe5b4736aad5
Yann Defretin <yann@defret.in>
2021-12-31 20:18:28 +0100

    fast-syntax-highlighting repo is hosted somewhere else now

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -17,7 +17,7 @@ if [[ $(grep /zsh$ /etc/shells | tail -1) ]]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
   # syntax highlightings
-  git clone https://github.com/zdharma/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
   # completions
   git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
