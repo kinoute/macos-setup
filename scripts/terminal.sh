09f71780e53d2225cbcbd50929024d336966b36f
Yann Defretin <yann@defret.in>
2022-11-14 21:41:26 +0100

    Add zsh/fish live autocompletions plugin

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -13,6 +13,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # syntax highlightings
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
+# fish live suggestion/completions
+git clone https://github.com/marlonrichert/zsh-autocomplete ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autocomplete
+
 # completions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
